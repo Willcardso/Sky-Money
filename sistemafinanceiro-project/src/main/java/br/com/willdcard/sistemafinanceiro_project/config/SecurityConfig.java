811f566e846e84335eadc67b2bea3093a5ab2136
Willcardso <114116371+Willcardso@users.noreply.github.com>
2025-12-14 13:53:49 -0300

    Add files via upload

---
+++ sistemafinanceiro-project/src/main/java/br/com/willdcard/sistemafinanceiro_project/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.willdcard.sistemafinanceiro_project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
