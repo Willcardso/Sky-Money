3cb01a28a19d29d756dc62a914f4ce514b41ff2e
Willcardso <114116371+Willcardso@users.noreply.github.com>
2026-01-12 19:49:54 -0300

    Delete sistemafinanceiro-project directory

--- sistemafinanceiro-project/src/main/java/br/com/willdcard/sistemafinanceiro_project/model/ExpenseType.java
+++
@@ -1,6 +0,0 @@
-package br.com.willdcard.sistemafinanceiro_project.model;
-
-public enum ExpenseType {
-    INCOME,
-    EXPENSE
-}
--- sistemafinanceiro-project/src/main/java/br/com/willdcard/sistemafinanceiro_project/service/DashboardService.java
+++
@@ -1,5 +0,0 @@
-package br.com.willdcard.sistemafinanceiro_project.service;
-
-public class DashboardService {
-
-}
